156558d0722f6498aac5df94fa70596b7d7f11d4
Anagreatti07 <129889849+Anagreatti07@users.noreply.github.com>
2023-04-05 12:23:33 -0300

    teste

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 document.addEventListener("click" , showcolor);
 
 document.getElementById("botao").addEventListener("click" , exibenome);
-document.getElementById("btn-soma").addEventListener("click" , exiberesultado);
 
 
 function showcolor()
@@ -14,11 +13,4 @@ function exibenome ()
     let usuario = document.getElementById("nome").value;
     document.getElementById("mensagem").innerHTML = "meu nome é:" +usuario;
     document.getElementById("nome").value = "";
-}
-
-function exiberesultado ()
-{
-    let usuario = document.getElementById("num1").value;
-    let usuario = document.getElementById("num2").value;
-    document.getElementById("resultado1").innerHTML = parseInt(num1) + parseInt(num2)
 }
