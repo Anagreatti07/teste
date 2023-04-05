30aa213d0d934b704b326005bd5cd879f216250f
Anagreatti07 <129889849+Anagreatti07@users.noreply.github.com>
2023-04-05 13:28:33 -0300

    teste8

--- main.js
+++ main.js
@@ -15,35 +15,35 @@ function showcolor()
 function exibenome ()
 {
     let usuario = document.getElementById("nome").value;
-    document.getElementById("mensagem").innerHTML = "meu nome é:" +usuario;
+    document.getElementById("mensagem").innerHTML = "meu nome é:" + usuario;
     document.getElementById("nome").value = "";
 }
 
 
 function exiberesultado1 ()
 {
-    let numero1 = document.getElementById("num1").value;
-    let numero2 = document.getElementById("num2").value;
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
     document.getElementById("resultado1").innerHTML = parseFloat(num1) + parseFloat(num2);
 }
 
 function exiberesultado2 ()
 {
-    let usuario = document.getElementById("num3").value;
-    let usuario = document.getElementById("num4").value;
+    let num3 = document.getElementById("num3").value;
+    let num4 = document.getElementById("num4").value;
     document.getElementById("resultado2").innerHTML = parseFloat(num3) + parseFloat(num4);
 }
 
 function exiberesultado3 ()
 {
-    let usuario = document.getElementById("num5").value;
-    let usuario = document.getElementById("num6").value;
+    let num5 = document.getElementById("num5").value;
+    let num6 = document.getElementById("num6").value;
     document.getElementById("resultado3").innerHTML = parseFloat(num5) + parseFloat(num6);
 }
 
 function exiberesultado4 ()
 {
-    let usuario = document.getElementById("num7").value;
-    let usuario = document.getElementById("num8").value;
+    let num7 = document.getElementById("num7").value;
+    let num8 = document.getElementById("num8").value;
     document.getElementById("resultado4").innerHTML = parseFloat(num7) + parseFloat(num8);
 }
