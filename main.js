4ed1545e7f5db1ba2b1257ef45f80d8719857056
Anagreatti07 <129889849+Anagreatti07@users.noreply.github.com>
2023-04-05 13:39:49 -0300

    teste9

--- main.js
+++ main.js
@@ -24,26 +24,26 @@ function exiberesultadosoma ()
 {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-    document.getElementById("resultado1").innerHTML = parseFloat(num1) + parseFloat(num2);
+    document.getElementById("resultadosoma").innerHTML = parseFloat(num1) + parseFloat(num2);
 }
 
 function exiberesultadodividir ()
 {
     let num3 = document.getElementById("num3").value;
     let num4 = document.getElementById("num4").value;
-    document.getElementById("resultado2").innerHTML = parseFloat(num3) / parseFloat(num4);
+    document.getElementById("resultadodividir").innerHTML = parseFloat(num3) / parseFloat(num4);
 }
 
 function exiberesultadomultiplicar ()
 {
     let num5 = document.getElementById("num5").value;
     let num6 = document.getElementById("num6").value;
-    document.getElementById("resultado3").innerHTML = parseFloat(num5) * parseFloat(num6);
+    document.getElementById("resultadomultiplicar").innerHTML = parseFloat(num5) * parseFloat(num6);
 }
 
 function exiberesultadosubtrair ()
 {
     let num7 = document.getElementById("num7").value;
     let num8 = document.getElementById("num8").value;
-    document.getElementById("resultado4").innerHTML = parseFloat(num7) - parseFloat(num8);
+    document.getElementById("resultadosubtrair").innerHTML = parseFloat(num7) - parseFloat(num8);
 }
