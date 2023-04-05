a6bf5f3d3d28aa6269b23bada0df03d1ca9e3551
Anagreatti07 <129889849+Anagreatti07@users.noreply.github.com>
2023-04-05 12:44:23 -0300

    teste

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 document.addEventListener("click" , showcolor);
 
 document.getElementById("botao").addEventListener("click" , exibenome);
-document.getElementById("botao").addEventListener("click" , exiberesultado1);
+document.getElementById("btn-soma").addEventListener("click" , exiberesultado1);
+document.getElementById("btn-dividir").addEventListener("click" , exiberesultado2);
+document.getElementById("btn-multiplicar").addEventListener("click" , exiberesultado3);
+document.getElementById("btn-subtrair").addEventListener("click" , exiberesultado4);
 
 
 function showcolor()
@@ -21,5 +24,26 @@ function exiberesultado1 ()
 {
     let usuario = document.getElementById("num1").value;
     let usuario = document.getElementById("num2").value;
-    document.getElementById("resultado1").innerHTML = "num1" + "num2";
+    document.getElementById("resultado1").innerHTML = parseFloat(num1) + parseFloat(num2);
+}
+
+function exiberesultado2 ()
+{
+    let usuario = document.getElementById("num3").value;
+    let usuario = document.getElementById("num4").value;
+    document.getElementById("resultado2").innerHTML = parseFloat(num3) + parseFloat(num4);
+}
+
+function exiberesultado3 ()
+{
+    let usuario = document.getElementById("num5").value;
+    let usuario = document.getElementById("num6").value;
+    document.getElementById("resultado3").innerHTML = parseFloat(num5) + parseFloat(num6);
+}
+
+function exiberesultado4 ()
+{
+    let usuario = document.getElementById("num7").value;
+    let usuario = document.getElementById("num8").value;
+    document.getElementById("resultado4").innerHTML = parseFloat(num7) + parseFloat(num8);
 }
