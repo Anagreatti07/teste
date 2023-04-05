452b43e913c355a9447318cf1f1eb88b317d6466
Anagreatti07 <129889849+Anagreatti07@users.noreply.github.com>
2023-04-05 13:36:59 -0300

    testando

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 document.addEventListener("click" , showcolor);
 
 document.getElementById("botao").addEventListener("click" , exibenome);
-document.getElementById("btn-soma").addEventListener("click" , exiberesultado1);
-document.getElementById("btn-dividir").addEventListener("click" , exiberesultado2);
-document.getElementById("btn-multiplicar").addEventListener("click" , exiberesultado3);
-document.getElementById("btn-subtrair").addEventListener("click" , exiberesultado4);
+document.getElementById("btn-soma").addEventListener("click" , exiberesultadosoma);
+document.getElementById("btn-dividir").addEventListener("click" , exiberesultadodividir);
+document.getElementById("btn-multiplicar").addEventListener("click" , exiberesultadomultiplicar);
+document.getElementById("btn-subtrair").addEventListener("click" , exiberesultadosubtrair);
 
 
 function showcolor()
@@ -20,28 +20,28 @@ function exibenome ()
 }
 
 
-function exiberesultado1 ()
+function exiberesultadosoma ()
 {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
     document.getElementById("resultado1").innerHTML = parseFloat(num1) + parseFloat(num2);
 }
 
-function exiberesultado2 ()
+function exiberesultadodividir ()
 {
     let num3 = document.getElementById("num3").value;
     let num4 = document.getElementById("num4").value;
     document.getElementById("resultado2").innerHTML = parseFloat(num3) / parseFloat(num4);
 }
 
-function exiberesultado3 ()
+function exiberesultadomultiplicar ()
 {
     let num5 = document.getElementById("num5").value;
     let num6 = document.getElementById("num6").value;
     document.getElementById("resultado3").innerHTML = parseFloat(num5) * parseFloat(num6);
 }
 
-function exiberesultado4 ()
+function exiberesultadosubtrair ()
 {
     let num7 = document.getElementById("num7").value;
     let num8 = document.getElementById("num8").value;
